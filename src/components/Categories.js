19493faf80ed552268bea98ec16920a82e306dff
radhe shyam <radhe212221@gmail.com>
2022-08-13 08:36:44 +0530

    cats css completed

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Categories() {
   const a = ["Hand Bags", "Watch", "Books", "Glasses"];
+  const [s, sets] = useState("Hand Bags");
   return (
     <div className="cats">
       {a.map((x) => (
-        <div className="item active">{x}</div>
+        <div onClick={()=>sets(x)} className={s === x ? "item active" : "item"}>{x}</div>
       ))}
     </div>
   );
